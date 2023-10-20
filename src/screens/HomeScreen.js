5dc1e1d839201ca33bef93e1956b8638843ebc6a
Pallavi Gupta <92811572+pallavi030@users.noreply.github.com>
2023-10-20 22:09:30 +0530

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -34,13 +34,13 @@ export default class HomeScreen extends Component {
     return (
         <React.Fragment>
             <Helmet>
-                <title>mehedi hassan.</title>
-                <meta name="description" content="A creative designer and developer based in London." />
+                <title>Pallavi Gupta.</title>
+                <meta name="description" content="A creative designer and developer based in India." />
             </Helmet>
 
             <div className={`wrapper ${this.props.location.state && this.props.location.state.comeThru ? "no-bg" : ""}`}>
                 <header>
-                    <Link to={{pathname: "/", state: { ...this.props.location.state, comeThru: true }}}><h1>mehedi hassan.</h1><span></span></Link>
+                    <Link to={{pathname: "/", state: { ...this.props.location.state, comeThru: true }}}><h1>Pallavi Gupta.</h1><span></span></Link>
                     <div>
                         <span></span><span className="alt"></span>
                     </div>
@@ -49,15 +49,15 @@ export default class HomeScreen extends Component {
                 <div className="main site-cont">
                     <div className="hero">
                         <h1 className="hi">hi! i'm mehedi.</h1>
-                        <h1>a compsci undergrad, software engineer, and designer based in london.</h1>
+                        <h1>a compsci undergrad, software engineer, and designer based in India.</h1>
                         <h2>i am passionate about building cutting-edge and elegant products designed to solve problems.</h2>
                         <Link to={{pathname: "/contact", state: { ...this.props.location.state, comeThru: true}}} className="services card">more about me <span className="arrow">-></span></Link>
                     </div>
 
                     <div className="children">
                         <div class="socials">
-                            <a target="_blank" href="https://github.com/MehediH">GitHub</a>
-                            <a target="_blank" href="https://www.linkedin.com/in/meh-hassan/">LinkedIn</a>
+                            <a target="_blank" href="https://github.com/pallavi030">GitHub</a>
+                            <a target="_blank" href="https://www.linkedin.com/in/pallavi-gupta-2035b2208/">LinkedIn</a>
                         </div>
                         <Link to={{pathname: "/projects", state: { ...this.props.location.state, comeThru: true}}}>
                             <div className="projects card">
